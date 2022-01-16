6751e392d9ef947a1ac8a4ce3913d058ce82c2e0
Nurdingans <96080159+Nurdingans@users.noreply.github.com>
2022-01-16 09:09:44 +0700

    Update sewa.js

--- shop/sewa.js
+++ shop/sewa.js
@@ -3,21 +3,21 @@ return `「 *PRICE LIST* 」
 							
 ⬣ PRICE SEWABOT
 
-⬡ 1 MINGGU = ${_minggu}
-⬡ 2 MINGGU = ${__minggu}
-⬡ 3 MINGGU = ${___minggu}
-⬡ 1 BULAN = ${_bulan}
-⬡ PERMANEN = ${_permanen}
-⬡ PERMANEN + USER PREM = ${__permanen}
+⬡ 1 MINGGU = ${1k minggu}
+⬡ 2 MINGGU = ${3k minggu}
+⬡ 3 MINGGU = ${5k minggu}
+⬡ 1 BULAN = ${8k bulan}
+⬡ PERMANEN = ${10k permanen}
+⬡ PERMANEN + USER PREM = ${ 15k permanen}
 
 
 ⬣ PRICE JADIBOT
 
-⬡ JADI BOT PERMANEN 25K
+⬡ JADI BOT PERMANEN 20K
 
 
 ⬣ MINAT? PM
-⬡ wa.me/62887435047326
+⬡ wa.me/6285950471442
 
 
 NOTE : 
